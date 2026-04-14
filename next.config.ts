bf50ceaf95dcad55fc094b868814155086b73b64
khanessah1-netizen <khanessah1@gmail.com>
2026-04-15 00:48:20 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/valentines',
+  assetPrefix: '/valentines',
   images: {
     unoptimized: true,
   },

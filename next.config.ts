eeb943e38783b5753359825ab680b2a94d6ecd2f
khanessah1-netizen <khanessah1@gmail.com>
2026-04-14 23:35:14 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  // Your Next.js config
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/valentines',
+  assetPrefix: '/valentines/',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
